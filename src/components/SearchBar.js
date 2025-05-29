3bf3ebc2099b6d05ecff77ccbcf5b2570ed15f60
washub <wumar42@gmail.com>
2025-05-29 17:24:36 +0530

    added media query on search component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,12 @@
 const SearchBar = ()=>{
     return (
-        <div className="search-feed self-center w-2/5">
+        <div className="search-feed w-3/5 self-center sm:w-2/5">
             <form>
-                <div className="input-field w-full flex justify-around">
+                <div className="input-field w-full flex justify-around flex-col sm:flex-row">
                     <input type="text" name="search" id="search"
-                      className="w-10/12 text-base pl-1 border border-solid border-black"/>
-                    <button className="px-4 text-lg bg-green-300 border border-solid border-green-800
-                    hover:bg-green-200">Search</button>
+                      className="w-full sm:w-10/12 text-base pl-1 border border-solid border-black"/>
+                    <button className="text-sm bg-green-300 border border-solid border-green-800
+                    hover:bg-green-200 mt-2  self-center sm:mt-0 sm:w-20 sm:text-lg">Search</button>
                 </div>
             </form>
         </div>
