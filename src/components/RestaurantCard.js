f9b677389b3aaf18ee42441cec40ea8d864cf4cf
washub <wumar42@gmail.com>
2025-05-29 16:19:44 +0530

    added media query for card and menu

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -5,7 +5,7 @@ import { ZWIGGY_IMG_CDN_URL } from "../common/constants"
 const RestaurantCard = (props)=>{
     const {cloudinaryImageId, name, aggregatedDiscountInfoV3, sla, cuisines, avgRating, description, areaName, id} = props?.restaurant?.info
     return (
-        <Link to={"/restaurant/"+id} className="anchor block h-64 w-1/5 border border-solid border-gray-500 overflow-hidden rounded-lg hover:scale-95 transition-transform">
+        <Link to={"/restaurant/"+id} className="anchor block h-64 w-4/6 sm:w-2/5 md:w-2/6 lg:w-1/5 border border-solid border-gray-500 overflow-hidden rounded-lg hover:scale-95 transition-transform">
             <div className="rest-card h-full w-full flex flex-col gap-2 justify-start">
                 <div className="rest-logo h-2/4 overflow-hidden relative rounded-lg">
                     <img src={ZWIGGY_IMG_CDN_URL + cloudinaryImageId} alt={name} className="h-full w-full object-cover"/>
@@ -14,14 +14,14 @@ const RestaurantCard = (props)=>{
                     </div>
                 </div>
                 <div className="rest-body h-2/4 flex flex-col justify-between pl-1">
-                    <h4 className="rest-title">{name}</h4>
+                    <h4 className="rest-title text-lg font-semibold">{name}</h4>
                     <div className="rating">
-                        <span>{avgRating}⭐ . </span>
-                        <span style={{fontWeight:"300"}}>{sla?.slaString} mins</span>
+                        <span className="font-light">{avgRating}⭐: </span>
+                        <span className="font-medium">{sla?.slaString}</span>
                     </div>
-                    <div className="cuisine text-base">{cuisines.join(", ")}</div>
+                    <div className="cuisine font-light text-xs">{cuisines.join(", ")}</div>
                     {/* <div className="description">{description}</div> */}
-                    <div className="res-address">{areaName}</div>
+                    <div className="res-address text-sm">{areaName}</div>
                 </div>
             </div>
         </Link>
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -15,30 +15,30 @@ const RestaurantMenu = ()=>{
     }
 
     return (
-        <div className="menu mt-4 flex flex-col gap-4 w-4/5 mx-auto">
+        <div className="menu mt-4 flex flex-col gap-4 w-4/5 lg:w-3/5 mx-auto">
             {restItem.map((res, ind) =>{
                return (
                  <div key={ind} className="menu-category">
-                    <div className="menu-title font-bold text-lg mb-3"><h3>{res.card.card.title} ({res?.card?.card?.itemCards?.length})</h3></div>
+                    <div className="menu-title font-bold text-xl mb-5"><h3>{res.card.card.title} ({res?.card?.card?.itemCards?.length})</h3></div>
                     {res?.card?.card?.itemCards?.map(item =>{
                         const {name, id, price,defaultPrice, ratings, description, itemAttribute, imageId, category} = item?.card?.info
                       
                         return (
                             <div className="menu-container flex flex-col" key={id}>
-                                <div className="menu-info h-44 w-full flex gap-3 items-start" key={id}>
-                                    <div className="menu-body w-3/4">
-                                        <div className={itemAttribute.vegClassifier==="NONVEG"?"nonveg":"veg"}></div>
-                                        <h5>{name}</h5>
+                                <div className="menu-info h-96 justify-evenly sm:h-44 w-full flex flex-col sm:flex-row sm:gap-3 items-start" key={id}>
+                                    <div className="menu-body w-full h-40 overflow-clip sm:w-3/4">
+                                        <div className={`w-3 h-3 rounded-sm ${itemAttribute.vegClassifier==="NONVEG"?"bg-red-500":"bg-green-600"}`}></div>
+                                        <h5 className="text-lg font-semibold">{name}</h5>
                                         <h6>₹{price/100|| defaultPrice/100}</h6>
                                         {/* {ratings.aggregatedRating.rating?'⭐' + ratings.aggregatedRating.rating +' (' + ratings.aggregatedRating.ratingCountV2+')':''}</h6> */}
                                         {ratings.aggregatedRating.rating? <h6>⭐ {ratings.aggregatedRating.rating} ({ratings.aggregatedRating.ratingCountV2})</h6>:<></>}
                                         <p>{description}</p>
                                     </div>
-                                    <div className="w-1/5 h-4/5 mx-auto">
+                                    <div className="w-2/4 h-44 sm:w-1/5 sm:h-4/5 mx-auto">
                                         {imageId ?<img className="menu-logo w-full h-full object-cover rounded-lg" src={ZWIGGY_IMG_CDN_URL+imageId} alt={category} />:<></>}
                                     </div>
                                  </div>
-                                 <div className="horizonal-line mb-4 border-b border-solid border-gray-500"></div>
+                                 <div className="horizonal-line mb-4 border-b border-solid border-gray-300"></div>
                             </div>
                         )
                     })}
