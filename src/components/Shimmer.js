876d85d7ccb8cdfb61e0c76afa4bd408a68d970a
washub <wumar42@gmail.com>
2025-05-29 14:54:29 +0530

    styled shimmer ui

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,41 +1,41 @@
 
 const Shimmer = ()=>{
     return (
-        <div className="shimmer-container">
-            <div className="shimmer-card">
-                <div className="shimmer-logo"></div>
-                <div className="shimmer-body"></div>
-                <div className="shimmer-desc"></div>
+        <div className="shimmer-container ml-56 flex flex-wrap w-10/12 gap-4">
+            <div className="shimmer-card flex flex-col gap-2 h-64 w-1/5  overflow-hidden rounded-lg">
+                <div className="shimmer-logo h-2/4 w-full rounded-lg bg-gray-200"></div>
+                <div className="shimmer-body w-full h-10 bg-gray-200 rounded-lg"></div>
+                <div className="shimmer-desc w-2/3  h-8 bg-gray-200 rounded-lg"></div>
             </div>
-            <div className="shimmer-card">
-                <div className="shimmer-logo"></div>
-                <div className="shimmer-body"></div>
-                <div className="shimmer-desc"></div>
+            <div className="shimmer-card flex flex-col gap-2 h-64 w-1/5  overflow-hidden rounded-lg">
+                <div className="shimmer-logo h-2/4 w-full rounded-lg bg-gray-200"></div>
+                <div className="shimmer-body w-full h-10 bg-gray-200 rounded-lg"></div>
+                <div className="shimmer-desc w-2/3  h-8 bg-gray-200 rounded-lg"></div>
             </div>
-            <div className="shimmer-card">
-                <div className="shimmer-logo"></div>
-                <div className="shimmer-body"></div>
-                <div className="shimmer-desc"></div>
+           <div className="shimmer-card flex flex-col gap-2 h-64 w-1/5  overflow-hidden rounded-lg">
+                <div className="shimmer-logo h-2/4 w-full rounded-lg bg-gray-200"></div>
+                <div className="shimmer-body w-full h-10 bg-gray-200 rounded-lg"></div>
+                <div className="shimmer-desc w-2/3  h-8 bg-gray-200 rounded-lg"></div>
             </div>
-            <div className="shimmer-card">
-                <div className="shimmer-logo"></div>
-                <div className="shimmer-body"></div>
-                <div className="shimmer-desc"></div>
+           <div className="shimmer-card flex flex-col gap-2 h-64 w-1/5  overflow-hidden rounded-lg">
+                <div className="shimmer-logo h-2/4 w-full rounded-lg bg-gray-200"></div>
+                <div className="shimmer-body w-full h-10 bg-gray-200 rounded-lg"></div>
+                <div className="shimmer-desc w-2/3  h-8 bg-gray-200 rounded-lg"></div>
             </div>
-            <div className="shimmer-card">
-                <div className="shimmer-logo"></div>
-                <div className="shimmer-body"></div>
-                <div className="shimmer-desc"></div>
+            <div className="shimmer-card flex flex-col gap-2 h-64 w-1/5  overflow-hidden rounded-lg">
+                <div className="shimmer-logo h-2/4 w-full rounded-lg bg-gray-200"></div>
+                <div className="shimmer-body w-full h-10 bg-gray-200 rounded-lg"></div>
+                <div className="shimmer-desc w-2/3  h-8 bg-gray-200 rounded-lg"></div>
             </div>
-            <div className="shimmer-card">
-                <div className="shimmer-logo"></div>
-                <div className="shimmer-body"></div>
-                <div className="shimmer-desc"></div>
+           <div className="shimmer-card flex flex-col gap-2 h-64 w-1/5  overflow-hidden rounded-lg">
+                <div className="shimmer-logo h-2/4 w-full rounded-lg bg-gray-200"></div>
+                <div className="shimmer-body w-full h-10 bg-gray-200 rounded-lg"></div>
+                <div className="shimmer-desc w-2/3  h-8 bg-gray-200 rounded-lg"></div>
             </div>
-            <div className="shimmer-card">
-                <div className="shimmer-logo"></div>
-                <div className="shimmer-body"></div>
-                <div className="shimmer-desc"></div>
+            <div className="shimmer-card flex flex-col gap-2 h-64 w-1/5  overflow-hidden rounded-lg">
+                <div className="shimmer-logo h-2/4 w-full rounded-lg bg-gray-200"></div>
+                <div className="shimmer-body w-full h-10 bg-gray-200 rounded-lg"></div>
+                <div className="shimmer-desc w-2/3  h-8 bg-gray-200 rounded-lg"></div>
             </div>
         </div>
     )
